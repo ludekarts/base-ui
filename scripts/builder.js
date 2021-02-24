b252c757c87e281549dab4e1490bb69b8a5ea4a9
ludekarts <ludekarts@gmail.com>
2021-02-24 11:38:59 +0100

    Another structure refactioring

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,8 +2,8 @@ const { runCommand } = require("@ludekarts/task-runner");
 
 (async function builder() {
   await runCommand("microbundle -i src/basic/index.js -o public/base-ui.js --no-pkg-main --jsx React.createElement -f cjs");
-  await runCommand("microbundle -i src/media/index.js -o public/lib/media.js --no-pkg-main --jsx React.createElement -f cjs");
-  await runCommand("microbundle -i src/panels/index.js -o public/lib/panels.js --no-pkg-main --jsx React.createElement -f cjs");
-  await runCommand("microbundle -i src/layout/index.js -o public/lib/layout.js --no-pkg-main --jsx React.createElement -f cjs");
-  await runCommand("microbundle -i src/buttons/index.js -o public/lib/buttons.js --no-pkg-main --jsx React.createElement -f cjs");
+  await runCommand("microbundle -i src/media/index.js -o public/media.js --no-pkg-main --jsx React.createElement -f cjs");
+  await runCommand("microbundle -i src/panels/index.js -o public/panels.js --no-pkg-main --jsx React.createElement -f cjs");
+  await runCommand("microbundle -i src/layout/index.js -o public/layout.js --no-pkg-main --jsx React.createElement -f cjs");
+  await runCommand("microbundle -i src/buttons/index.js -o public/buttons.js --no-pkg-main --jsx React.createElement -f cjs");
 }());
